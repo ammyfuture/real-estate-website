bee6e504817ceea3a8194c60b1c276c4ae0a0602
Amy Saeed <ammythecreator@gmail.com>
2023-11-27 07:18:01 -0800

    Set up the connection to the database

--- server-of-real-estate/index.js
+++ server-of-real-estate/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// .then and catch to see if we are successfully connected and if not why
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
