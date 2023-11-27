74abac01cd59491e8f932c78649d844f632a2ec6
Amy Saeed <ammythecreator@gmail.com>
2023-11-27 10:49:36 -0800

    Created the sign up api route

---
+++ server-of-real-estate/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+export const signup = async (req, res) => {
+  console.log(req.body);
+  const { username, email, password } = req.body;
+  // no need for await here, hashSync is already using it
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    // must use await, takes time
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
--- server-of-real-estate/index.js
+++ server-of-real-estate/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-
+import authRouter from "./routes/auth.route.js";
 // .then and catch to see if we are successfully connected and if not why
 mongoose
   .connect(process.env.MONGO)
@@ -12,8 +12,12 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/auth", authRouter);
