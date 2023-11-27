f4cd71cdf3175c6ad407d655b4151df628a2a4c0
Amy Saeed <ammythecreator@gmail.com>
2023-11-27 03:01:57 -0800

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home, Profile, SignIn, SignUp, About } from "./pages";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
