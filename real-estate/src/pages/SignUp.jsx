d8ffed4c0802aa0f8865954f7a90b9da7a20921d
Amy Saeed <ammythecreator@gmail.com>
2023-11-28 07:06:30 -0800

    95% done with signin page ui and functionality

--- real-estate/src/pages/SignUp.jsx
+++ real-estate/src/pages/SignUp.jsx
@@ -79,7 +79,7 @@ const SignUp = () => {
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to="/sign-in">
-          <span className="text-blue-700">Sign In</span>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
