5fbd188ca53439c3fa5bef14a749ea612f0b738e
Amy Saeed <ammythecreator@gmail.com>
2023-11-28 08:38:04 -0800

    Added redux persist functionality

--- real-estate/src/pages/SignIn.jsx
+++ real-estate/src/pages/SignIn.jsx
@@ -7,26 +7,20 @@ import {
   signInFailure,
 } from "../redux/user/userSlice";
 
-const SignIn = () => {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(false);
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
-      // setLoading(true);
       dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
@@ -35,30 +29,22 @@ const SignIn = () => {
         },
         body: JSON.stringify(formData),
       });
-
       const data = await res.json();
-
+      console.log(data);
       if (data.success === false) {
-        // setLoading(false);
-        // setError(data.message);
         dispatch(signInFailure(data.message));
         return;
       }
-      // setLoading(false);
-      // setError(null);
-      dispatch(signInSuccess());
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      // setLoading(false);
-      // setError(err.message);
       dispatch(signInFailure(error.message));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="email"
           placeholder="email"
@@ -66,7 +52,6 @@ const SignIn = () => {
           id="email"
           onChange={handleChange}
         />
-
         <input
           type="password"
           placeholder="password"
@@ -74,6 +59,7 @@ const SignIn = () => {
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
@@ -82,13 +68,12 @@ const SignIn = () => {
         </button>
       </form>
       <div className="flex gap-2 mt-5">
-        <p>Dont Have an account?</p>
-        <Link to="/sign-up">
+        <p>Dont have an account?</p>
+        <Link to={"/sign-up"}>
           <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
-};
-export default SignIn;
+}
