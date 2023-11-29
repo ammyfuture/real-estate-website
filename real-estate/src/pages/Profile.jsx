24e80563ebb872f44db8a5f9218998f23aa5813d
Amy Saeed <ammythecreator@gmail.com>
2023-11-29 04:43:29 -0800

    Created user update api

--- real-estate/src/pages/Profile.jsx
+++ real-estate/src/pages/Profile.jsx
@@ -1,5 +1,125 @@
+// import { useSelector } from "react-redux";
+// import { Link } from "react-router-dom";
+// import { useRef, useState, useEffect } from "react";
+// import {
+//   getDownloadURL,
+//   getStorage,
+//   ref,
+//   uploadBytesResumable,
+// } from "firebase/storage";
+// import { app } from "../firebase";
+// const Profile = () => {
+//   const fileRef = useRef(null);
+//   const { currentUser } = useSelector((state) => state.user);
+//   const [file, setFile] = useState(undefined);
+//   const [filePerc, setFilePerc] = useState(0);
+//   const [fileUploadError, setFileUploadError] = useState(false);
+//   const [formData, setFormData] = useState({});
+//   const { username, email, avatar } = currentUser;
+
+//   useEffect(() => {
+//     if (file) {
+//       handleFileUpload(file);
+//     }
+//   }, [file]);
+
+//   const handleFileUpload = (file) => {
+//     const storage = getStorage(app);
+//     const fileName = new Date().getTime() + file.name;
+//     const storageRef = ref(storage, fileName);
+//     const uploadTask = uploadBytesResumable(storageRef, file);
+
+//     uploadTask.on(
+//       "state_changed",
+//       (snapshot) => {
+//         const progress =
+//           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//         console.log("Upload is" + progress + "% done");
+//         setFilePerc(Math.round(progress));
+//       },
+//       (error) => {
+//         setFileUploadError(true);
+//       },
+//       () => {
+//         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+//           setFormData({ ...formData, avatar: downloadURL });
+//         });
+//       }
+//     );
+//   };
+//   return (
+//     <div className="p-3 max-w-lg mx-auto">
+//       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+
+//       <form className="flex flex-col gap-4">
+//         <input
+//           type="file"
+//           ref={fileRef}
+//           hidden
+//           accept="image/*"
+//           onChange={(e) => setFile(e.target.files[0])}
+//         />
+//         <img
+//           onClick={() => fileRef.current.click()}
+//           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 "
+//           src={formData.avatar || avatar}
+//           alt="profile image"
+//         />
+
+//         <p className="text-sm self-center">
+//           {fileUploadError ? (
+//             <span className="text-red-700">
+//               Error Image Upload (image must be less than 2 mb
+//             </span>
+//           ) : filePerc > 0 && filePerc < 100 ? (
+//             <span className="text-slate-700">{`Uploading${filePerc}`}</span>
+//           ) : filePerc === 100 ? (
+//             <span className="text-green-700">Image successfully uploaded!</span>
+//           ) : (
+//             ""
+//           )}
+//         </p>
+
+//         <input
+//           className="border p-3 rounded-lg"
+//           type="text"
+//           placeholder={username}
+//           id="text"
+//           // onChange={handleChange}
+//         />
+
+//         <input
+//           type="email"
+//           placeholder={email}
+//           className="border p-3 rounded-lg"
+//           id="email"
+//           // onChange={handleChange}
+//         />
+
+//         <input
+//           type="password"
+//           placeholder="password"
+//           className="border p-3 rounded-lg"
+//           id="password"
+//           // onChange={handleChange}
+//         />
+//         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+//           update
+//         </button>
+//         <button>create listing</button>
+//       </form>
+
+//       <div className="flex justify-between mt-5">
+//         <span className="text-red-700 cursor-pointer">Delete account</span>
+//         <span className="text-red-700 cursor-pointer">Sign out</span>
+//       </div>
+//       <Link>Show Listing</Link>
+//     </div>
+//   );
+// };
+// export default Profile;
+
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { useRef, useState, useEffect } from "react";
 import {
   getDownloadURL,
@@ -8,14 +128,21 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-const Profile = () => {
+
+import { Link } from "react-router-dom";
+export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  const { username, email, avatar } = currentUser;
+
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
 
   useEffect(() => {
     if (file) {
@@ -34,87 +161,95 @@ const Profile = () => {
       (snapshot) => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is" + progress + "% done");
         setFilePerc(Math.round(progress));
       },
       (error) => {
         setFileUploadError(true);
       },
       () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setFormData({ ...formData, avatar: downloadURL });
-        });
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
       }
     );
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-
       <form className="flex flex-col gap-4">
         <input
+          onChange={(e) => setFile(e.target.files[0])}
           type="file"
           ref={fileRef}
           hidden
           accept="image/*"
-          onChange={(e) => setFile(e.target.files[0])}
         />
         <img
           onClick={() => fileRef.current.click()}
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 "
-          src={formData.avatar || avatar}
-          alt="profile image"
+          src={formData.avatar || currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
-
         <p className="text-sm self-center">
           {fileUploadError ? (
             <span className="text-red-700">
-              Error Image Upload (image must be less than 2 mb
+              Error Image upload (image must be less than 2 mb)
             </span>
           ) : filePerc > 0 && filePerc < 100 ? (
-            <span className="text-slate-700">{`Uploading${filePerc}`}</span>
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
           ) : filePerc === 100 ? (
             <span className="text-green-700">Image successfully uploaded!</span>
           ) : (
             ""
           )}
         </p>
-
         <input
-          className="border p-3 rounded-lg"
           type="text"
-          placeholder={username}
-          id="text"
-          // onChange={handleChange}
+          placeholder="username"
+          defaultValue={currentUser.username}
+          id="username"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
-
         <input
           type="email"
-          placeholder={email}
-          className="border p-3 rounded-lg"
+          placeholder="email"
           id="email"
-          // onChange={handleChange}
+          defaultValue={currentUser.email}
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
-
         <input
           type="password"
           placeholder="password"
-          className="border p-3 rounded-lg"
+          onChange={handleChange}
           id="password"
-          // onChange={handleChange}
+          className="border p-3 rounded-lg"
         />
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          update
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
-        <button>create listing</button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
-
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
-      <Link>Show Listing</Link>
+
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
     </div>
   );
-};
-export default Profile;
+}
--- server-of-real-estate/index.js
+++ server-of-real-estate/index.js
@@ -1,9 +1,50 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import authRouter from "./routes/auth.route.js";
+// import userRoute from "./routes/user.route.js";
+// import cookieParser from "cookie-parser";
+// // .then and catch to see if we are successfully connected and if not why
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log("Connected to MongoDB!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const app = express();
+// app.use(express.json());
+// app.use(cookieParser);
+
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+
+// app.use("/api/auth", authRouter);
+// app.use("/api/user", userRoute);
+
+// // this func is looking for a status code and message, we get it from the error coming if mongoDB couldn't save. Otherwise we have our default to fall back on
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   return res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-// .then and catch to see if we are successfully connected and if not why
+import cookieParser from "cookie-parser";
+dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -14,15 +55,18 @@ mongoose
   });
 
 const app = express();
+
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
 
+app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-// this func is looking for a status code and message, we get it from the error coming if mongoDB couldn't save. Otherwise we have our default to fall back on
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
