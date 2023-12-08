a5c2ab79b04e0b8dca1ad1e2956945d74a7c810e
Amy Saeed <ammythecreator@gmail.com>
2023-12-08 09:22:08 -0800

    Added contact landlord component to the listing page

---
+++ real-estate/src/components/Contact.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Contact = ({ listing }) => {
+  const { userRef, name } = listing;
+  console.log(userRef, name);
+  const [landLord, setLandLord] = useState(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchLandLord = async () => {
+      try {
+        const res = await fetch(`/api/user/${userRef}`);
+        const data = await res.json();
+        setLandLord(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchLandLord();
+  }, [userRef]);
+
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
+  return (
+    <>
+      {landLord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{landLord.username}</span>{" "}
+            for <span>{name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            rows="2"
+            value={message}
+            onChange={onChange}
+            placeholder="Enter your message here..."
+            className="w-full border p-3 rounded"
+          ></textarea>
+
+          <Link
+            to={`mailto:${landLord.email}?subject=Regrading${name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+export default Contact;
