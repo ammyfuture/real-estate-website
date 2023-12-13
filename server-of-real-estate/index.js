2dc06fbe48937d06a9e0e40911fb5b79a19a4eb4
Amy Saeed <ammythecreator@gmail.com>
2023-12-12 16:38:13 -0800

    Added render scripts and pre install config

--- server-of-real-estate/index.js
+++ server-of-real-estate/index.js
@@ -1,48 +1,10 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
-// import authRouter from "./routes/auth.route.js";
-// import userRoute from "./routes/user.route.js";
-// import cookieParser from "cookie-parser";
-// // .then and catch to see if we are successfully connected and if not why
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(() => {
-//     console.log("Connected to MongoDB!");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// const app = express();
-// app.use(express.json());
-// app.use(cookieParser);
-
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
-
-// app.use("/api/auth", authRouter);
-// app.use("/api/user", userRoute);
-
-// // this func is looking for a status code and message, we get it from the error coming if mongoDB couldn't save. Otherwise we have our default to fall back on
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || "Internal Server Error";
-//   return res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -55,6 +17,7 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -69,6 +32,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/real-estate/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "real-estate", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
