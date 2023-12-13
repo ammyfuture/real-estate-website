1ab945b931704c9aae2e8ed78bfc9a38f2462cb1
Amy Saeed <ammythecreator@gmail.com>
2023-12-12 16:55:31 -0800

    Updated json files and index in the server

--- server-of-real-estate/index.js
+++ server-of-real-estate/index.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-import path from "path";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -18,6 +18,7 @@ mongoose
   });
 
 const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, "/real-estate/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "real-estate", "dist", "index.html"));
 });
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
